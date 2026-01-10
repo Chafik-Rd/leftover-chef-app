32a236bedfe187aec5ee056000f59658ef00b7cc
Chafik-Rd <78787036+Chafik-Rd@users.noreply.github.com>
2026-01-10 14:26:41 +0700

    fix(auth store): edit logout and name type

--- src/store/auth.store.ts
+++ src/store/auth.store.ts
@@ -1,21 +1,26 @@
 import { AuthService } from "@/services/auth.service";
 import { create } from "zustand";
+import { useUserIngreStore } from "./user-ingredient.store";
+
 interface User {
   firstName: string;
   email: string;
   role: string;
 }
-interface AuthStrore {
+interface AuthStore {
   user: User | null;
   setUser: (user: User) => void;
   logout: () => void;
 }
-export const useAuthStore = create<AuthStrore>((set) => ({
+export const useAuthStore = create<AuthStore>((set) => ({
   user: null,
   setUser: (user) => set({ user }),
   logout: () =>
     set(() => {
       AuthService.logout();
+      const { clearIngredients } = useUserIngreStore.getState();
+      clearIngredients();
+      localStorage.removeItem('is_logged_in');
       return { user: null };
     }),
 }));
